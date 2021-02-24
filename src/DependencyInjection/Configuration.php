acded47bb2db500d7be1585a2721d77f0cc5e2f2
Maxime Huran <m.huran@monsieurbiz.com>
2021-02-24 18:13:12 +0100

    Init plugin

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAntiSpamPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        return new TreeBuilder('monsieurbiz_sylius_anti_spam');
+    }
+}
