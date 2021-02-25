5ec153a0e32bea6060c037037fe58c523a0e7f95
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2021-02-25 23:34:03 +0100

    Add ReCaptcha3 validator (empty for now)

---
+++ src/Validator/ReCaptcha3Validator.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAntiSpamPlugin\Validator;
+
+final class ReCaptcha3Validator implements ValidatorInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @TODO implement
+     */
+    public function isEligible(object $object, array $options = []): bool
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @TODO implement
+     */
+    public function validate(object $object, array $options = []): array
+    {
+        return [];
+    }
+}
