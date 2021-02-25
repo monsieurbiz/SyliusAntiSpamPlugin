1784d5d6d8852517752cf4f024cfd9b60ed86ffa
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2021-02-25 14:13:10 +0100

    The customer can now fetch it's own quarantine item

---
+++ src/Entity/CustomerQuarantineItemAwareTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAntiSpamPlugin\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait CustomerQuarantineItemAwareTrait
+{
+    /**
+     * @ORM\OneToOne(targetEntity="\MonsieurBiz\SyliusAntiSpamPlugin\Entity\QuarantineItemInterface", mappedBy="customer")
+     *
+     * @var QuarantineItemInterface|null
+     */
+    protected ?QuarantineItemInterface $quarantineItem = null;
+
+    /**
+     * @return QuarantineItemInterface|null
+     */
+    public function getQuarantineItem(): ?QuarantineItemInterface
+    {
+        return $this->quarantineItem;
+    }
+
+    /**
+     * @param QuarantineItemInterface|null $quarantineItem
+     */
+    public function setQuarantineItem(?QuarantineItemInterface $quarantineItem): void
+    {
+        $this->quarantineItem = $quarantineItem;
+    }
+}
---
+++ tests/Application/src/Entity/Customer.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use MonsieurBiz\SyliusAntiSpamPlugin\Entity\CustomerQuarantineItemAwareTrait;
+use Sylius\Component\Core\Model\Customer as SyliusCustomer;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_customer")
+ */
+class Customer extends SyliusCustomer
+{
+    use CustomerQuarantineItemAwareTrait;
+}
