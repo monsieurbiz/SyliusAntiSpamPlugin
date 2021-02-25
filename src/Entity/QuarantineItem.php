7e8889f48129a17e9387ea7917ce87f106533510
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2021-02-25 12:39:30 +0100

    Setup the QuarantineItem entity

---
+++ src/Entity/QuarantineItem.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAntiSpamPlugin\Entity;
+
+use Sylius\Component\Customer\Model\CustomerInterface;
+use Sylius\Component\Resource\Model\TimestampableTrait;
+
+class QuarantineItem implements QuarantineItemInterface
+{
+    use TimestampableTrait;
+
+    /**
+     * @var int|null
+     */
+    private ?int $id = null;
+
+    /**
+     * @var CustomerInterface|null
+     */
+    private ?CustomerInterface $customer = null;
+
+    /**
+     * @var string|null
+     */
+    private ?string $reasonCode = null;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCustomer(): ?CustomerInterface
+    {
+        return $this->customer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCustomer(?CustomerInterface $customer): void
+    {
+        $this->customer = $customer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReasonCode(): ?string
+    {
+        return $this->reasonCode;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setReasonCode(?string $reasonCode): void
+    {
+        $this->reasonCode = $reasonCode;
+    }
+}
---
+++ src/Entity/QuarantineItemInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAntiSpamPlugin\Entity;
+
+use Sylius\Component\Customer\Model\CustomerInterface;
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+interface QuarantineItemInterface extends ResourceInterface
+{
+    /**
+     * @return CustomerInterface|null
+     */
+    public function getCustomer(): ?CustomerInterface;
+
+    /**
+     * @param CustomerInterface|null $customer
+     */
+    public function setCustomer(?CustomerInterface $customer): void;
+
+    /**
+     * @return string|null
+     */
+    public function getReasonCode(): ?string;
+
+    /**
+     * @param string|null $reasonCode
+     */
+    public function setReasonCode(?string $reasonCode): void;
+}
