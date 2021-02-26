9f235f67ddf4c9411845778090d88b05cc519798
Maxime Huran <m.huran@monsieurbiz.com>
2021-02-26 17:21:15 +0100

    Add HealthChecker class to create Quarantine item if necessary

---
+++ src/Checker/HealthChecker.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAntiSpamPlugin\Checker;
+
+use MonsieurBiz\SyliusAntiSpamPlugin\Entity\QuarantineItemAwareInterface;
+use MonsieurBiz\SyliusAntiSpamPlugin\Entity\QuarantineItemInterface;
+use MonsieurBiz\SyliusAntiSpamPlugin\Registry\ValidatorsRegistryInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+
+class HealthChecker implements HealthCheckerInterface
+{
+    private ValidatorsRegistryInterface $validatorsRegistry;
+    private FactoryInterface $quarantineItemFactory;
+
+    /**
+     * @param ValidatorsRegistryInterface $validatorsRegistry
+     * @param FactoryInterface $quarantineItemFactory
+     */
+    public function __construct(
+        ValidatorsRegistryInterface $validatorsRegistry,
+        FactoryInterface $quarantineItemFactory
+    ) {
+        $this->validatorsRegistry = $validatorsRegistry;
+        $this->quarantineItemFactory = $quarantineItemFactory;
+    }
+
+    /**
+     * @param QuarantineItemAwareInterface $object
+     *
+     * @return void
+     */
+    public function check(QuarantineItemAwareInterface $object): void
+    {
+        $reasonCodes = $this->validate($object);
+        if (empty($reasonCodes)) {
+            return;
+        }
+
+        /** @var QuarantineItemInterface $quarantineItem */
+        $quarantineItem = $this->quarantineItemFactory->createNew();
+        $quarantineItem->setReasonCodes($reasonCodes);
+        $quarantineItem->setEmail($object->getEmail());
+        $quarantineItem->setLevel(QuarantineItemInterface::LEVEL_SUSPECTED);
+        $object->setQuarantineItem($quarantineItem);
+    }
+
+    /**
+     * @param QuarantineItemAwareInterface $object
+     *
+     * @return string[]
+     */
+    private function validate(QuarantineItemAwareInterface $object): array
+    {
+        $reasonCodes = [];
+        foreach ($this->validatorsRegistry->getValidators() as $validator) {
+            if (!$validator->isEligible($object)) {
+                continue;
+            }
+
+            $reasonCodes = array_merge($reasonCodes, $validator->validate($object));
+        }
+
+        return $reasonCodes;
+    }
+}
