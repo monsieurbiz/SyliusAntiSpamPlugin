624a9bf6cbc07dc91c17541504910e321e38d658
Maxime Huran <m.huran@monsieurbiz.com>
2021-03-01 17:45:44 +0100

    Make ReCaptcha validator check Recaptcha score if enabled

--- src/Validator/ReCaptcha3Validator.php
+++ src/Validator/ReCaptcha3Validator.php
@@ -13,8 +13,31 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusAntiSpamPlugin\Validator;
 
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3Validator as Recaptcha3ValidatorConstraints;
+use ReCaptcha\Response as RecaptchaResponse;
+
 final class ReCaptcha3Validator implements ValidatorInterface
 {
+    const CAPTCHA_MINIMUM_SCORE = 0.8;
+
+    /**
+     * @var Recaptcha3ValidatorConstraints
+     */
+    private Recaptcha3ValidatorConstraints $recaptcha3ValidatorConstraint;
+
+    /**
+     * @var bool
+     */
+    private bool $karserRecaptcha3Enabled;
+
+    public function __construct(
+        Recaptcha3ValidatorConstraints $recaptcha3ValidatorConstraint,
+        bool $karserRecaptcha3Enabled
+    ) {
+        $this->recaptcha3ValidatorConstraint = $recaptcha3ValidatorConstraint;
+        $this->karserRecaptcha3Enabled = $karserRecaptcha3Enabled;
+    }
+
     /**
      * @inheritdoc
      *
@@ -23,7 +46,7 @@ final class ReCaptcha3Validator implements ValidatorInterface
      */
     public function isEligible(object $object, array $options = []): bool
     {
-        return false;
+        return $this->karserRecaptcha3Enabled;
     }
 
     /**
@@ -34,6 +57,15 @@ final class ReCaptcha3Validator implements ValidatorInterface
      */
     public function validate(object $object, array $options = []): array
     {
-        return [];
+        $errors = [];
+
+        $lastResponse = $this->recaptcha3ValidatorConstraint->getLastResponse();
+        if (!$lastResponse instanceof RecaptchaResponse) {
+            $errors[] = 'monsieurbiz_anti_spam.error.missing_captcha_reponse';
+        } elseif ($lastResponse->getScore() < self::CAPTCHA_MINIMUM_SCORE) {
+            $errors[] = 'monsieurbiz_anti_spam.error.invalid_captcha_score';
+        }
+
+        return $errors;
     }
 }
