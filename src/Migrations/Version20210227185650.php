298b62168b084981ed0fe7912d36b2e475616885
Maxime Huran <m.huran@monsieurbiz.com>
2021-03-01 10:43:58 +0100

    Correct PHP CS

--- src/Migrations/Version20210227185650.php
+++ src/Migrations/Version20210227185650.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusAntiSpamPlugin\Migrations;
@@ -12,18 +21,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210227185650 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE monsieurbiz_anti_spam_quarantine_item CHANGE reason_codes reason_codes LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE monsieurbiz_anti_spam_quarantine_item CHANGE reason_codes reason_codes LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:array)\'');
