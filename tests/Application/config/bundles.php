df97d72840be4f66286386fe13d47081c2acf74b
Maxime Huran <m.huran@monsieurbiz.com>
2021-03-22 18:30:16 +0100

    Add new test application using symlinks when we can

---
+++ tests/Application/config/bundles.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+return array_merge(
+    require dirname(__DIR__) . '/../../vendor/sylius/sylius/config/bundles.php',
+    [
+        MonsieurBiz\SyliusAntiSpamPlugin\MonsieurBizSyliusAntiSpamPlugin::class => ['all' => true],
+    ]
+);
