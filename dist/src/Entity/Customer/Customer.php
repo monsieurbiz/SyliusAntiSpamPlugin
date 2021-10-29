48bb79005c9218351a18dca2f28856932365afa2
Maxime Huran <m.huran@monsieurbiz.com>
2021-10-29 16:27:31 +0200

    Add dist files

---
+++ dist/src/Entity/Customer/Customer.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Anti Spam plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Entity\Customer;
+
+use Doctrine\ORM\Mapping as ORM;
+use MonsieurBiz\SyliusAntiSpamPlugin\Entity\QuarantineItemAwareInterface;
+use MonsieurBiz\SyliusAntiSpamPlugin\Entity\QuarantineItemAwareTrait;
+use Sylius\Component\Core\Model\Customer as SyliusCustomer;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_customer")
+ */
+class Customer extends SyliusCustomer implements QuarantineItemAwareInterface
+{
+    use QuarantineItemAwareTrait;
+}
