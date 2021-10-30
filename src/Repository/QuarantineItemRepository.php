f5ec35b18e81e81722ff2081c3c24a13fb296b44
Maxime Huran <m.huran@monsieurbiz.com>
2021-10-30 12:08:18 +0200

    Correct PHP Stan return for QuarantineItemRepository

--- src/Repository/QuarantineItemRepository.php
+++ src/Repository/QuarantineItemRepository.php
@@ -13,8 +13,12 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusAntiSpamPlugin\Repository;
 
+use MonsieurBiz\SyliusAntiSpamPlugin\Entity\QuarantineItemInterface;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 
+/**
+ * @method QuarantineItemInterface[] findBy(array $criteria, ?array $orderBy = null, $limit = null, $offset = null)
+ */
 final class QuarantineItemRepository extends EntityRepository implements QuarantineItemRepositoryInterface
 {
     /**
