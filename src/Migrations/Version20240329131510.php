3cf93ea7896d3beff03f7a6943d068769a76f79a
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2024-03-29 14:17:28 +0100

    chore: add migration to change json type

---
+++ src/Migrations/Version20240329131510.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAntiSpamPlugin\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240329131510 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Change json_array(deprecated) to json type';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE monsieurbiz_anti_spam_quarantine_item CHANGE reason_codes reason_codes JSON NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE monsieurbiz_anti_spam_quarantine_item CHANGE reason_codes reason_codes LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\'');
+    }
+}
